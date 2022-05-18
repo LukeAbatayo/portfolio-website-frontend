58b10f4506f84e42bb8fe554eee16d9228d03354
Luke Abatayo <luke.abatayo17@gmail.com>
2022-05-19 00:46:03 +0800

    added slider and social media anchors on sections

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
-        {['home', 'about', 'contact', 'work', 'skills', 'contact'].map((item) => (
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
@@ -26,14 +26,15 @@ const Navbar = () => {
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
         {toggle && (
-          <motion.div whileInView={{ x: [300, 0] }} transition={{ duration: 0.85, ease: 'easeOut' }}>
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {['home', 'about', 'contact', 'work', 'skills', 'contact'].map((item) => (
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
                 <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
                 </li>
               ))}
             </ul>
